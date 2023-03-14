40926b0d3ca7971866306bb24cdae97ece6894a3
Rajab Karume <rkarume@gmail.com>
2023-03-14 16:07:40 +0300

    ✨created the input field

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import NavBar from './components/Mainpage/Navbar/NavBar';
+import Input from './components/Mainpage/Input/Input';
+// import NavBar from './components/Mainpage/Navbar/NavBar';
 // import LogIn from './components/Login/LogIn';
 
 function App() {
   return (
     <div className="App">
       {/* <LogIn /> */}
-      <NavBar />
+      {/* <NavBar /> */}
+      <Input />
     </div>
   );
 }
---
+++ src/components/Mainpage/Input/Input.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Input.css'
+
+function Input(){
+    
+    return(
+        <div>
+            <form>
+                <h2>Journal entry</h2>
+                <input type='text' placeholder="Enter your Journal " />
+            </form>
+            <button>Send</button>
+        </div>
+    )
+}
+
+export default Input
