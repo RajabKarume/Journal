1e64decc82a6c0e71376ead4cdc740abb4b7a2d8
Rajab Karume <rkarume@gmail.com>
2023-03-14 21:28:45 +0300

    ✨created mainpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Card from './components/Mainpage/Card/Card';
+import MainPage from './components/Mainpage/Display/MainPage';
+// import Card from './components/Mainpage/Card/Card';
 // import Input from './components/Mainpage/Input/Input';
 // import NavBar from './components/Mainpage/Navbar/NavBar';
 // import LogIn from './components/Login/LogIn';
@@ -10,7 +11,8 @@ function App() {
       {/* <LogIn /> */}
       {/* <NavBar /> */}
       {/* <Input /> */}
-      <Card />
+      {/* <Card /> */}
+      <MainPage />
     </div>
   );
 }
