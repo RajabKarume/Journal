1510cf5a870182dd2564622ffa1d7028a081050a
Rajab Karume <rkarume@gmail.com>
2023-04-05 15:26:30 +0300

    ✨ added nodemailer to firebase functions

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,41 @@ const admin = require("firebase-admin");
 
 
 admin.initializeApp();
-// const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
+// const { error } = require("firebase-functions/logger");
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.REACT_APP_EMAIL,
+    pass: process.env.REACT_APP_PASSWORD,
+  },
+});
+
+exports.sendEmail = functions.firestore
+    .document(`entries/{userId}`)
+    .onCreate((snap, context)=>{
+      const mailOption = {
+        from: `myJournal@noreply.com`,
+        to: snap.data().email,
+        subject: `entries`,
+        html: `<p>${snap.data().newEntry}</p>`,
+      };
+      return transporter.sendMail(mailOption, (error, data)=>{
+        if (error) {
+          console.log(error);
+          return;
+        }
+        console.log("Sent");
+      });
+    });
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
